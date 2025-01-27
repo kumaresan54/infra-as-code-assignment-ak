0bba03027c51b018cd5402e6ae8f95f8b300f7c0
kumaresan.anandhan <kumaresan.anandhan@thoughtworks.com>
2025-01-27 23:00:35 +0530

    linting changes

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,17 @@
 provider "aws" {
   region = var.region
 }
+
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0, < 4.0.0"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.2.0, < 3.0.0"
+    }
+  }
+}
