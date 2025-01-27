08b21044af101271b88952f7d7768dafa48c6699
kumaresan.anandhan <kumaresan.anandhan@thoughtworks.com>
2025-01-27 23:15:45 +0530

    separate oidc flow and add stages for terraform apply

---
+++ terraform/oidc/oidc_iam.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role" "github_actions_role" {
+  name = "GitHubActionsRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_oidc.arn
+        }
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:sub" = "repo:kumaresan54/infra-as-code-assignment-ak:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
+
+
+resource "aws_iam_policy" "github_actions_policy" {
+  name        = "GitHubActionsPolicy"
+  description = "Policy for GitHub Actions to deploy infrastructure"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:PutObject",
+          "ec2:Describe*",
+          "ec2:TerminateInstances"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_policy_attachment" {
+  policy_arn = aws_iam_policy.github_actions_policy.arn
+  role       = aws_iam_role.github_actions_role.name
+}
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.0.0, < 4.0.0"
+      version = ">= 5.0.0, < 6.0.0"
     }
     archive = {
       source  = "hashicorp/archive"
