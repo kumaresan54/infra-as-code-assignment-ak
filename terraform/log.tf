eb7b61540804d9575e7e350723e82da632e6a5ff
kumaresan.anandhan <kumaresan.anandhan@thoughtworks.com>
2025-01-28 00:19:43 +0530

    formatting fix

--- terraform/log.tf
+++ terraform/log.tf
@@ -1,8 +1,4 @@
 resource "aws_cloudwatch_log_group" "lambda_logs" {
   for_each = local.lambda_functions
-  name = "/aws/lambda/${each.key}-${var.prefix}"
+  name     = "/aws/lambda/${each.key}-${var.prefix}"
 }
-
-# resource "aws_cloudwatch_log_group" "verify_user_logs" {
-#   name = "/aws/lambda/verify_user"
-# }
